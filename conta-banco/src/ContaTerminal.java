b3de8a4731d6f061636fa83afda06f61e47b79eb
luiscspalmeira <luiscspalmeira@gmail.com>
2024-05-27 19:09:56 -0300

     Simulando Uma Conta Bancária Através Do Terminal Console

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,32 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        Scanner teclado = new Scanner(System.in);
+
+        //DecimalFormat formato = new DecimalFormat("#.##");
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        //int numero = Integer.parseInt(teclado.nextLine());
+        Integer numero = Integer.valueOf(teclado.nextLine());
+
+        System.out.println("Por favor, digite o nome da Agência !");
+        String nomeAg = teclado.nextLine();
+
+        System.out.println("Por favor, digite o seu nome !");
+        String nome = teclado.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo !");
+        double saldo = Double.parseDouble(teclado.nextLine());
+        //saldo = Double.valueOf(formato.format(saldo));
+
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+        + nomeAg + " , conta " + numero + " e seu saldo " + new DecimalFormat("#,##0.00").format(saldo) + " já está disponível para saque.");
+
+        teclado.close();
+
+    }
+}
