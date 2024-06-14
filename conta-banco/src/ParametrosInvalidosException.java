c03e4f48ea325325045d1d83b794b74d1cdaca67
luiscspalmeira <luiscspalmeira@gmail.com>
2024-06-14 08:51:13 -0300

    Exluidos os exercicios

--- conta-banco/src/ParametrosInvalidosException.java
+++
@@ -1,8 +0,0 @@
-public class ParametrosInvalidosException extends Exception{
-
-    @Override
-    public String toString() {
-        return "Numero de conta invalido. Digite exatamente 8 digitos.";
-    }
-    
-}
